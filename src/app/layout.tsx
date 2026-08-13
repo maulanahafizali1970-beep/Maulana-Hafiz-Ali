909519c268aaff97ba7a593963fad8c556392c1c
maulanahafizali1970-beep <maulanahafizali1970-beep@users.noreply.github.com>
2026-08-13 22:36:53 +0530

    Add Tawk.to live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,22 @@ export default function RootLayout({
             }),
           }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a7df6502850431d47cd920a/1jvu0kb3s';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+`,
+          }}
+        />
       </body>
     </html>
   );
